3c9460411961092a1a56d2a5a984d0f00b6a6c0c
Kyuwoong Lee <kwooler@gmail.com>
2023-10-15 21:11:04 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+def bubblesort(number):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+
+def bubble(number):
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
+
+
+def main():
+    number = list(map(int, input().split()))
+    bubblesort(number)
+    print(number)
+
+
+if __name__ == '__main__':
+    main()
